9370f37320b5a8d73bcda23bdc7563b3d4e6eaeb
Errorman76 <lwooyoung57@gmail.com>
2025-01-14 19:52:34 +0900

    [wip] updating extra (language)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- lang: rust
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
+-- extra: linting > eslint
+vim.g.lazyvim_eslint_auto_format = true
+
+-- extra: formatting > prettier
+vim.g.lazyvim_prettier_needs_config = false
---
+++ lua/langs/omnisharp.lua
@@ -0,0 +1,64 @@
+return {
+{
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      cs = { "csharpier" },
+    },
+    formatters = {
+      csharpier = {
+        command = "dotnet-csharpier",
+        args = { "--write-stdout" },
+      },
+    },
+  },
+},
+{
+  "mfussenegger/nvim-dap",
+  optional = true,
+  opts = function()
+    local dap = require("dap")
+    if not dap.adapters["netcoredbg"] then
+      require("dap").adapters["netcoredbg"] = {
+        type = "executable",
+        command = vim.fn.exepath("netcoredbg"),
+        args = { "--interpreter=vscode" },
+        options = {
+          detached = false,
+        },
+      }
+    end
+    for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
+      if not dap.configurations[lang] then
+        dap.configurations[lang] = {
+          {
+            type = "netcoredbg",
+            name = "Launch file",
+            request = "launch",
+            ---@diagnostic disable-next-line: redundant-parameter
+            program = function()
+              return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end
+  end,
+},
+ {
+  "nvim-neotest/neotest",
+  optional = true,
+  dependencies = {
+    "Issafalcon/neotest-dotnet",
+  },
+  opts = {
+    adapters = {
+      ["neotest-dotnet"] = {
+        -- Here we can set options for neotest-dotnet
+      },
+    },
+  },
+}
+}
