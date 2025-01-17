16a3baaf62fa5e34b61bf5e6b388f7c98ccdd2e5
Errorman76 <lwooyoung57@gmail.com>
2025-01-17 21:52:41 +0900

    [update] add keymap for blink.cmp and change fzf window opt

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,12 @@
+return {
+  -- remap keymap for select snippet
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+    },
+  },
+}
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,4 +6,16 @@ return {
       scroll = { enabled = false },
     },
   },
+
+  -- resize picker
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "echasnovski/mini.icons" },
+    opts = {
+      winopts = {
+        height = 0.95,
+        width = 0.95,
+      },
+    },
+  },
 }
