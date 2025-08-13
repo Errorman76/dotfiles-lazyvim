cc06e2ad01f3cf3e45f54395647b01248b4734ef
Errorman76 <lwooyoung57@gmail.com>
2025-08-13 22:53:53 +0900

    [update] add svelt option

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = {},
+        javascript = {},
+        html = {},
+        css = {},
+      },
+    },
+  },
+}
